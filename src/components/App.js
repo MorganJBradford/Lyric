68a70be7b2a4c70551f52770b18043ebbf026559
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-10 14:03:44 -0700

    Add necessary packages, begin basic project structure
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+import './App.css';
+
+function App() {
+return (
+    <>
+      <Header/>
+      <MainContent />
+    </>
+  );
+}
+
+export default App;
