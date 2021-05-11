8548354349c6258ef2d2fba88bd1256deb6080cf
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-11 13:28:42 -0500

    add container. Add searchbar Form
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {ReactComponent as Background} from "./Test.svg";
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container';
 
 function Home() {
   return(
     <>
-      <Background/>
+      <Background />
+      <Container className="container-position">
+        <Form className="form-style">
+          <Form.Group controlId="formBasicSearch">
+            <Form.Control size="lg" type="text" placeholder="Search..."></Form.Control>
+            <Form.Text className="form-text">
+              Search by artist, song or album
+            </Form.Text>
+          </Form.Group>
+        </Form>
+      </Container>
     </>
   )
 }
