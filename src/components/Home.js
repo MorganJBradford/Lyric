d086db51a44f6ba7a85a617486bbbf5414290b78
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-12 16:36:39 -0500

    create handleSearch and render Artists methods
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>
    Co-authored-by: Tien Nguyen <tien96ng@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {ReactComponent as Background} from "./Test.svg";
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Artist from "./Artist";
 import PropTypes from 'prop-types';
+import { AppContext } from "../AppContext";
+import ArtistApiCall from "./ArtistApiCall";
 
-function Home(props) {
+function Home() {
+  const { search, setSearch } = useContext(AppContext);
+
+  const handleSearch = e => {
+    e.preventDefault();
+    let artist = e.target.search.value.replace(" ", "%20");
+    setSearch(artist);
+    renderArtists();
+  }
+
+  const renderArtists = () => {
+    if(search === null) {
+      return <></>
+    } else {
+      return <ArtistApiCall />
+    }
+  }
+
   return(
     <>
       <Background />
-      <Container className="container-position" onSubmit={props.handleSearchSubmit}>
-        <Form className="form-style">
-          <Form.Group controlId="formBasicSearch">
-            <Form.Control size="lg" type="text" placeholder="Search..." name="search" className="input-color"></Form.Control>
-            <Form.Text className="form-text">
-              Search by Artist
-            </Form.Text>
-          </Form.Group>
-        </Form>
-        <Artist />
+      <Container className="container-position">
+        <Row>
+          <Col lg={6}>
+            <Form className="form-style" onSubmit={handleSearch}>
+              <Form.Group controlId="formBasicSearch">
+                <Form.Control size="lg" type="text" placeholder="Search..." name="search" className="input-color"></Form.Control>
+                <Form.Text className="form-text">
+                  Search by Artist
+                </Form.Text>
+              </Form.Group>                                
+              <Button size="lg" type="submit" className="button-color">                                                            
+                Search!
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          {renderArtists()}  
+        </Row>
       </Container>
     </>
   );
