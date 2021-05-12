b9e7810e8ccf7d11a5bbf51d66e5d3a673dcaf4b
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-12 17:44:04 -0500

    add animationState state slice
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- src/AppContext.js
+++ src/AppContext.js
@@ -5,13 +5,16 @@ export const AppContext = React.createContext();
 export const AppProvider = ({children}) => {
   const [artistList, setArtistList] = useState(null);
   const [search, setSearch] = useState(null);
+  const [animationState, setAnimationState] = useState(false);
 
   return (
     <AppContext.Provider value={{
         artistList,
         setArtistList,
         search,
-        setSearch
+        setSearch,
+        animationState,
+        setAnimationState
       }}
     >
       {children}
