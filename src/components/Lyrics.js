bacbd4449974fb86b8fc3cc28b17ac3e93a06dbb
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-11 21:06:49 -0500

    setup file structure. Setup local state
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

---
+++ src/components/Lyrics.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Lyrics() {
+  return(
+    <h1>Hello</h1>
+  )
+}
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,18 +1,53 @@
 import React from "react";
 import Header from "./Header";
 import Home from './Home';
-// import Footer from "./Footer";
+import SongList from './SongList';
+import ArtistList from './ArtistList';
+import AlbumList from './AlbumList';
+import Lyrics from './Lyrics';
+import Footer from "./Footer";
 
-function MainContent() {
+class MainContent extends React.Component {
 
-  let currentlyVisibleState = <Home/>;
-  
-  return (
-    <div className="bgImage">
-      <Header/>
-      {currentlyVisibleState}
-    </div>
-  );
+  constructor(props){
+    super(props)
+    this.state = {
+      splashVisible: true,
+      artistListVisible: false,
+      songListVisible: false,
+      lyricsVisible: false,
+      albumListVisible: false,
+    }
+  }
+
+
+  handleSearchSubmit = (e) => {
+    e.preventDefault();
+    const search = e.target.search.value;
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+
+    if(this.state.artistListVisible != false) {
+      currentlyVisibleState = <ArtistList/>
+    } else if (this.state.albumListVisible != false) {
+      currentlyVisibleState = <AlbumList/>
+    } else if(this.state.songListVisible != false) {
+      currentlyVisibleState = <SongList/>
+    } else if(this.state.lyricsVisible != false){
+      currentlyVisibleState = <Lyrics/>
+    } else {
+      currentlyVisibleState = <Home handleSearchSubmit={this.handleSearchSubmit}/>
+    }
+    return (
+      <div className="bgImage">
+        <Header/>
+        {currentlyVisibleState}
+        <Footer/>
+      </div>
+    )
+  }
 }
 
 
