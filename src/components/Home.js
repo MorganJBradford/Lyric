7bb0db025ebae00dccb9b657a61b3794e2e5f660
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-11 21:10:12 -0500

    create/style footer. add proptypes to home
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,30 @@ import React from 'react';
 import {ReactComponent as Background} from "./Test.svg";
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container';
+import Artist from "./Artist";
+import PropTypes from 'prop-types';
 
-function Home() {
+function Home(props) {
   return(
     <>
       <Background />
-      <Container className="container-position">
+      <Container className="container-position" onSubmit={props.handleSearchSubmit}>
         <Form className="form-style">
           <Form.Group controlId="formBasicSearch">
-            <Form.Control size="lg" type="text" placeholder="Search..." className="input-color"></Form.Control>
+            <Form.Control size="lg" type="text" placeholder="Search..." name="search" className="input-color"></Form.Control>
             <Form.Text className="form-text">
-              Search by artist, song or album
+              Search by Artist
             </Form.Text>
           </Form.Group>
         </Form>
+        <Artist />
       </Container>
     </>
-  )
+  );
+}
+
+Home.propTypes = {
+  handleSearchSubmit: PropTypes.func
 }
 
 export default Home;
