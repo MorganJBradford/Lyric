c954cc4ae9ba2af4310a53dc84619522618da580
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-12 16:34:41 -0500

    remove console.logs; move comments to bottom
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- src/components/ArtistApiCall.js
+++ src/components/ArtistApiCall.js
@@ -1,17 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AppContext } from "../AppContext";
-// import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
-// import useAxios from "axios-hooks";
 import axios from "axios";
 import jsonpAdapter from "axios-jsonp";
-
-
-/*
-setTimeout(() => {
-  
-}, 3000)
-
-*/
 
 function ArtistApiCall() {
   const {artistList, setArtistList, search} = useContext(AppContext);
@@ -19,17 +9,13 @@ function ArtistApiCall() {
 
   useEffect(() => {
       axios({
-        // artist=FIRST_NAME20%LAST_NAME
         url: `https://api.musixmatch.com/ws/1.1/artist.search?format=jsonp&callback=callback&q_artist=${search}&apikey=${process.env.REACT_APP_MUSIX_API_KEY}`,
         adapter: jsonpAdapter
       }).then(res => {
-        console.log(res);
           const {artist_list} = res.data.message.body;
           console.log(artist_list);
-          // console.log(res.data.message.body.artist_list);
           setArtistList(artist_list);
           setLoading(false);
-          console.log(search);
         })
         .catch(err => {
           console.log(err)
@@ -46,6 +32,14 @@ function ArtistApiCall() {
 
 export default ArtistApiCall;
 
+
+/*
+setTimeout(() => {
+  
+}, 3000)
+
+*/
+
 /*
 
 async function getArtist(search) {
@@ -58,3 +52,6 @@ async function getArtist(search) {
 }
 
 */
+
+// import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
+// import useAxios from "axios-hooks";
