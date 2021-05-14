9f42f68a2d9a28ff9517ed3f7b6a2ef2aa5d1772
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-13 19:07:21 -0700

    Create more state slices to make micro managing easier
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,18 +3,35 @@ import React, { useState } from "react";
 export const AppContext = React.createContext();
 
 export const AppProvider = ({children}) => {
-  const [artistList, setArtistList] = useState(null);
-  const [search, setSearch] = useState(null);
+  const [albumList, setAlbumList] = useState(null);
   const [animationState, setAnimationState] = useState(false);
+  const [artistList, setArtistList] = useState(null);
+  const [queryType, setQueryType] = useState(null);
+  const [search, setSearch] = useState(null);
+  const [searchMethod, setSearchMethod] = useState(null);
+  const [trackList, setTrackList] = useState(null);
+  const [track, setTrack] = useState(null);
+
 
   return (
-    <AppContext.Provider value={{
+    <AppContext.Provider value=
+    {{
+        albumList,
+        setAlbumList,  
+        animationState,
+        setAnimationState,
         artistList,
         setArtistList,
+        queryType,
+        setQueryType,
         search,
         setSearch,
-        animationState,
-        setAnimationState
+        searchMethod,
+        setSearchMethod,
+        trackList,
+        setTrackList,
+        track,
+        setTrack
       }}
     >
       {children}
