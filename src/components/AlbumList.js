4461c31c5fa63d9b7da2d65e815b6b19f69e70e3
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-13 19:08:49 -0700

    Create album and album list components, delete song components
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AppContext } from "../AppContext";
+import Row from 'react-bootstrap/Row';
+import Album from "./Album";
+import '../App.css';
+import HandleApiCall from "./HandleApiCall";
 
-export default function AlbumList() {
-  return(
-    <h1>Hello</h1>
-  )
+function AlbumList(props) {
+  const { albumList, setSearchMethod, setQueryType, setSearch } = useContext(AppContext);
+
+  const handleClick = (id) => {
+    setQueryType("album_id");
+    setSearch(id);
+    setSearchMethod("album.tracks.get");
+    renderAlbums();
+  }
+
+  const renderAlbums = () => {
+    return <HandleApiCall />
+  }
+
+
+  if (albumList !== null) {
+    return (
+      <>
+        <Row className="list-alignment">
+        <h3 className="name-alignment albums">Albums:</h3>
+          {albumList.map((album, index) =>
+          <div className="albums" onClick={() => handleClick(albumList[index].album.album_id)}>
+            <Album 
+            names={albumList[index].album.album_name}
+            key={index}/>
+          </div>
+          )}
+        </Row>
+      </>
+    );
+  } else {
+    return (
+      <>
+      </>
+    );
+  }
 }
+
+export default AlbumList;
